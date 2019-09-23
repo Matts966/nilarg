a010b176725905a0658615891c6dae890d68342c
Matts966 <Matts966@users.noreply.github.com>
2019-09-24 00:45:11 +0900

    Add panic call check

--- nilarg.go
+++ nilarg.go
@@ -3,7 +3,6 @@ package nilarg
 import (
 	"go/token"
 	"go/types"
-	"log"
 	"math/big"
 	"reflect"
 
@@ -30,12 +29,11 @@ instructions that refer the arguments are not reachable.
 `
 
 var Analyzer = &analysis.Analyzer{
-	Name:       "nilarg",
-	Doc:        Doc,
-	Run:        run,
-	Requires:   []*analysis.Analyzer{buildssa.Analyzer},
-	FactTypes:  []analysis.Fact{new(panicArgs), new(pkgDone)},
-	ResultType: reflect.TypeOf([]analysis.ObjectFact{}),
+	Name:      "nilarg",
+	Doc:       Doc,
+	Run:       run,
+	Requires:  []*analysis.Analyzer{buildssa.Analyzer},
+	FactTypes: []analysis.Fact{new(panicArgs), new(pkgDone)},
 }
 
 // panicArgs has the information about arguments which causes panic on
@@ -59,10 +57,15 @@ func run(pass *analysis.Pass) (interface{}, error) {
 		}
 		if cc == 0 {
 			pass.ExportPackageFact(&pkgDone{})
-			log.Println(pass.AllObjectFacts())
-			return pass.AllObjectFacts(), nil
+			break
 		}
 	}
+
+	for _, fn := range ssainput.SrcFuncs {
+		runFunc(pass, fn)
+	}
+
+	return nil, nil
 }
 
 // This function checkFunc checks all the nillable type arguments of
@@ -239,4 +242,171 @@ func isNilChecked(v *ssa.Parameter, b *ssa.BasicBlock, visited *big.Int) bool {
 func isNil(value ssa.Value) bool {
 	v, ok := value.(*ssa.Const)
 	return ok && v.IsNil()
+}
+
+func runFunc(pass *analysis.Pass, fn *ssa.Function) {
+	seen := make([]bool, len(fn.Blocks))
+	var visit func(b *ssa.BasicBlock, stack []fact)
+	visit = func(b *ssa.BasicBlock, stack []fact) {
+		if seen[b.Index] {
+			return
+		}
+		seen[b.Index] = true
+
+		// Report calls that can cause panic.
+		for _, instr := range b.Instrs {
+			if c, ok := instr.(*ssa.Call); ok {
+				s := c.Call.StaticCallee()
+				if s == nil || s.Object() == nil {
+					continue
+				}
+				var fact panicArgs
+				if pass.ImportObjectFact(s.Object(), &fact) {
+					for i := range fact {
+						if nilnessOf(stack, c.Common().Args[i]) == isnil {
+							pass.Reportf(c.Pos(), "this call can cause panic")
+						}
+					}
+				}
+			}
+		}
+
+		// For nil comparison blocks, report an error if the condition
+		// is degenerate, and push a nilness fact on the stack when
+		// visiting its true and false successor blocks.
+		if binop, tsucc, fsucc := eq(b); binop != nil {
+			xnil := nilnessOf(stack, binop.X)
+			ynil := nilnessOf(stack, binop.Y)
+			if ynil != unknown && xnil != unknown && (xnil == isnil || ynil == isnil) {
+				// If tsucc's or fsucc's sole incoming edge is impossible,
+				// it is unreachable.  Prune traversal of it and
+				// all the blocks it dominates.
+				// (We could be more precise with full dataflow
+				// analysis of control-flow joins.)
+				var skip *ssa.BasicBlock
+				if xnil == ynil {
+					skip = fsucc
+				} else {
+					skip = tsucc
+				}
+				for _, d := range b.Dominees() {
+					if d == skip && len(d.Preds) == 1 {
+						continue
+					}
+					visit(d, stack)
+				}
+				return
+			}
+
+			// "if x == nil" or "if nil == y" condition; x, y are unknown.
+			if xnil == isnil || ynil == isnil {
+				var f fact
+				if xnil == isnil {
+					// x is nil, y is unknown:
+					// t successor learns y is nil.
+					f = fact{binop.Y, isnil}
+				} else {
+					// x is nil, y is unknown:
+					// t successor learns x is nil.
+					f = fact{binop.X, isnil}
+				}
+
+				for _, d := range b.Dominees() {
+					// Successor blocks learn a fact
+					// only at non-critical edges.
+					// (We could do be more precise with full dataflow
+					// analysis of control-flow joins.)
+					s := stack
+					if len(d.Preds) == 1 {
+						if d == tsucc {
+							s = append(s, f)
+						} else if d == fsucc {
+							s = append(s, f.negate())
+						}
+					}
+					visit(d, s)
+				}
+				return
+			}
+		}
+
+		for _, d := range b.Dominees() {
+			visit(d, stack)
+		}
+	}
+
+	if fn.Blocks != nil {
+		visit(fn.Blocks[0], make([]fact, 0, 20)) // 20 is plenty
+	}
+}
+
+// A fact records that a block is dominated
+// by the condition v == nil or v != nil.
+type fact struct {
+	value   ssa.Value
+	nilness nilness
+}
+
+func (f fact) negate() fact { return fact{f.value, -f.nilness} }
+
+type nilness int
+
+const (
+	isnonnil         = -1
+	unknown  nilness = 0
+	isnil            = 1
+)
+
+var nilnessStrings = []string{"non-nil", "unknown", "nil"}
+
+func (n nilness) String() string { return nilnessStrings[n+1] }
+
+// nilnessOf reports whether v is definitely nil, definitely not nil,
+// or unknown given the dominating stack of facts.
+func nilnessOf(stack []fact, v ssa.Value) nilness {
+	// Is value intrinsically nil or non-nil?
+	switch v := v.(type) {
+	case *ssa.Alloc,
+		*ssa.FieldAddr,
+		*ssa.FreeVar,
+		*ssa.Function,
+		*ssa.Global,
+		*ssa.IndexAddr,
+		*ssa.MakeChan,
+		*ssa.MakeClosure,
+		*ssa.MakeInterface,
+		*ssa.MakeMap,
+		*ssa.MakeSlice:
+		return isnonnil
+	case *ssa.Const:
+		if v.IsNil() {
+			return isnil
+		} else {
+			return isnonnil
+		}
+	}
+
+	// Search dominating control-flow facts.
+	for _, f := range stack {
+		if f.value == v {
+			return f.nilness
+		}
+	}
+	return unknown
+}
+
+// If b ends with an equality comparison, eq returns the operation and
+// its true (equal) and false (not equal) successors.
+func eq(b *ssa.BasicBlock) (op *ssa.BinOp, tsucc, fsucc *ssa.BasicBlock) {
+	if If, ok := b.Instrs[len(b.Instrs)-1].(*ssa.If); ok {
+		if binop, ok := If.Cond.(*ssa.BinOp); ok {
+			switch binop.Op {
+			case token.EQL:
+				return binop, b.Succs[0], b.Succs[1]
+			case token.NEQ:
+				return binop, b.Succs[1], b.Succs[0]
+			}
+		}
+	}
+	return nil, nil, nil
 }
